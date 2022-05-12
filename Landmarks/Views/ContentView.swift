aadeb6feedb6ee2109381323301b8276c939a20a
thecuriouscat_meow <thecuriouscat_meow@icloud.com>
2022-05-12 08:23:48 +0530

    Composing Complex Interfaces (1,2 of 6)

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+            case featured
+            case list
+        }
+    
+    
     var body: some View {
         LandmarkList()
         
